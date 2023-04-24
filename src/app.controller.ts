4877acf4ec8348e9d8782d85c91a1c3a6026ee58
joel <whdduf972@gmail.com>
2023-04-24 13:12:47 +0900

    docs : project setup

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+// 컨트롤러 파일 (라우터)
+
+// 컨트롤러 데코레이터 : 라우팅 경로 매핑
+@Controller('/') 
+// 클래스로 패킹
+export class AppController {
+	constructor(private readonly appService: AppService) {}
+  // 컨스트럭터, 생성자 : 
+
+  // http 메서드 데코레이터
+  @Get() // 패스 매핑 가능
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// 네스트펙토리의 핵심기능을 사용하여 어플리케이션 인스턴스를 생성하는 엔트리파일
+// **listen을 보아 서버를 오픈하는 시작 파일**
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.SERVER_PORT);
+}
+
+bootstrap();
